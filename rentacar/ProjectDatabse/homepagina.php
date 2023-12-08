b2dd3349508577372b3db7ccc2ccc367ee04e40f
Malik Al Ibrahim <makimonj@gmail.com>
2023-12-08 13:31:39 +0100

    update

--- rentacar/ProjectDatabse/homepagina.php
+++ rentacar/ProjectDatabse/homepagina.php
@@ -6,7 +6,7 @@
     <title>Demo</title> 
    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-GLhlTQ8iUc1SZ3q6ZfQr+OpOiS460HWSl5Ll6aZO5e/Z9AnYX2Q+Brdd6zL2T2U" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    
 </head>
 <body>
     <nav>
