217055adad5532f86d85a1e1c02034a513e82ebc
Malik Al Ibrahim <makimonj@gmail.com>
2023-12-08 13:33:53 +0100

    update

--- rentacar/ProjectDatabse/homepagina.php
+++ rentacar/ProjectDatabse/homepagina.php
@@ -6,13 +6,14 @@
     <title>Demo</title> 
    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-GLhlTQ8iUc1SZ3q6ZfQr+OpOiS460HWSl5Ll6aZO5e/Z9AnYX2Q+Brdd6zL2T2U" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
     <nav>
         <a href="homepagina.php"><img src="Haima-logo.jpg" alt="logo" class="logo"></a>
         <ul>
             <li><a href="homepagina.php">Home</a></li>
+            <li><a href="services.php">Services</a></li>
             <li><a href="services.php">Services</a></li>
    
             <?php
